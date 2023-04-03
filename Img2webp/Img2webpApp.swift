4338cbe7e596672d51524cc2b3df7247238a9eda
givebest <givenlovs@msn.com>
2023-04-03 20:32:40 +0800

    Initial Commit

---
+++ Img2webp/Img2webpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Img2webpApp.swift
+//  Img2webp
+//
+//  Created by pei wang on 2023/4/3.
+//
+
+import SwiftUI
+
+@main
+struct Img2webpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
